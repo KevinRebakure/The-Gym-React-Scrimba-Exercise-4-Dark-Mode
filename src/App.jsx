68cff73a137bc0b7b79214d49b7b46fcdbf7c3f8
Rebakure <kevinrebakure@gmail.com>
2024-07-23 10:56:15 +0200

    organised files

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,3 @@
-import { useState } from "react";
-import WindowTracker from "./WindowTracker";
-
 export default function App() {
-  const [show, setShow] = useState(true);
-
-  function handleClick() {
-    setShow((prev) => !prev);
-    console.log(`The state is: ${show}`);
-  }
-
-  return (
-    <>
-      <div className="h-[100vh] space-y-5 bg-orange-500 p-10 text-center">
-        <button
-          onClick={handleClick}
-          className="rounded-xl bg-gray-100 px-5 py-3 transition-all duration-100 hover:bg-gray-700 hover:text-white"
-        >
-          Toggle WindowTracker
-        </button>
-        {show && <WindowTracker />}
-      </div>
-    </>
-  );
+  return <h1>Cool exercises😁</h1>;
 }
